32fefd0c5bf094a3d29e8793f03e0cf512d2724e
Alexis0003 <A342110014@unidem.edu.mx>
2023-11-20 16:48:49 -0600

    f
    f

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 class Program
@@ -36,8 +37,13 @@ class Program
                         string contenido = File.ReadAllText(rutaArchivo);
                         Console.WriteLine(contenido);
 
-                        // Verificar y mostrar la estructura de los ciclos
                         MostrarResultadoVerificacion(contenido);
+
+                        // Realizar análisis sintáctico adicional
+                        if (!VerificarSintaxis(contenido))
+                        {
+                            Console.WriteLine("El código tiene errores de sintaxis.");
+                        }
                     }
                     else
                     {
@@ -70,12 +76,10 @@ class Program
 
     static void MostrarResultadoVerificacion(string texto)
     {
-        // Expresiones regulares específicas para cada tipo de ciclo
-        string patronSi = @"(?<!\w)si\s([^)])\s{\s.;\s}";
-        string patronMientras = @"(?<!\w)mientras\s([^)])\s{\s.;\s}";
-        string patronPara = @"(?<!\w)para\s([^;];\s[^;];\s[^)])\s{\s.;\s}";
+        string patronSi = @"(?<!\w)si\s*\([^)]\)\s\{\s*.;\s\}";
+        string patronMientras = @"(?<!\w)mientras\s*\([^)]\)\s\{\s*.;\s\}";
+        string patronPara = @"(?<!\w)para\s*\([^;];\s[^;];\s[^)]\)\s\{\s*.;\s\}";
 
-        // Verificar y mostrar la estructura de los ciclos
         if (Regex.IsMatch(texto, patronSi))
         {
             Console.WriteLine("El ciclo 'si' está bien estructurado en el archivo.");
@@ -93,4 +97,74 @@ class Program
             Console.WriteLine("No se encontraron ciclos bien estructurados en el archivo.");
         }
     }
+
+    static bool VerificarSintaxis(string codigo)
+    {
+        int countLlaveAbierta = 0;
+        int countLlaveCerrada = 0;
+        int countParentesisAbierto = 0;
+        int countParentesisCerrado = 0;
+        int countComillas = 0;
+        int countPuntoComa = 0;
+
+        foreach (char c in codigo)
+        {
+            switch (c)
+            {
+                case '{':
+                    countLlaveAbierta++;
+                    break;
+                case '}':
+                    countLlaveCerrada++;
+                    break;
+                case '(':
+                    countParentesisAbierto++;
+                    break;
+                case ')':
+                    countParentesisCerrado++;
+                    break;
+                case '"':
+                    countComillas++;
+                    break;
+                case ';':
+                    countPuntoComa++;
+                    break;
+            }
+        }
+
+        bool sintaxisCorrecta = true;
+
+        if (countLlaveAbierta != countLlaveCerrada)
+        {
+            Console.WriteLine("Error: falta de llaves.");
+            sintaxisCorrecta = false;
+        }
+
+        if (countParentesisAbierto != countParentesisCerrado)
+        {
+            Console.WriteLine("Error: falta de paréntesis.");
+            sintaxisCorrecta = false;
+        }
+
+        if (countComillas % 2 != 0)
+        {
+            Console.WriteLine("Error: falta de comillas.");
+            sintaxisCorrecta = false;
+        }
+
+        if (countPuntoComa != 1)
+        {
+            Console.WriteLine("Error: falta punto y coma.");
+            sintaxisCorrecta = false;
+        }
+
+        if (sintaxisCorrecta)
+        {
+            Console.WriteLine("La sintaxis es correcta.");
+        }
+
+        return sintaxisCorrecta;
+    }
+
+
 }
